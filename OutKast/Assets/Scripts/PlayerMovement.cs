f475d50369493eb9e82485535bf4ecacebb7898f
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-03-11 11:04:32 +1100

    created Prefab and Scripts folder with player movement script
    The player movement script has basic functionality at the moment and will be worked on more

---
+++ OutKast/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	Rigidbody2D playerRB;
+	float horizontalInput;
+	public float playerSpeed;
+
+    void Start()
+    {
+		playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+		horizontalInput = Input.GetAxis("Horizontal");
+		Vector2 playerMovementVector = new Vector2(horizontalInput * playerSpeed, playerRB.velocity.y);
+		playerRB.AddForce(playerMovementVector * Time.deltaTime);
+    }
+}
