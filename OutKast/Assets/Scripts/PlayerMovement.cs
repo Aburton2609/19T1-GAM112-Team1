e19a00504e0cd7d71d46ec96f48bb2d52e1f56c4
Tony G <ssj.tony89@gmail.com>
2019-03-11 11:28:20 +1100

    fixed conflict

--- OutKast/Assets/Scripts/PlayerMovement.cs
+++ OutKast/Assets/Scripts/PlayerMovement.cs
@@ -8,18 +8,14 @@ public class PlayerMovement : MonoBehaviour
 	private float horizontalInput;
 	private float playerSpeed;
 
-    void Start()
-    {
+	void Start()
+	{
 		playerRB = GetComponent<Rigidbody2D>();
-    }
+	}
 
-    void Update()
-    {
+	void Update()
+	{
 		horizontalInput = Input.GetAxis("Horizontal");
 		playerRB.AddForce(new Vector2(horizontalInput * playerSpeed * Time.deltaTime, playerRB.velocity.y));
-<<<<<<< HEAD
-    }
-=======
-    }   
->>>>>>> adf84c0863db31e279f481b3370adf592b98bb14
+	}
 }
