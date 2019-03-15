243a9ba8c5480b62048ca207808fe29d47152e45
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-03-15 23:04:52 +1100

    I added the Light, LightTimer script, Orbs of Light and darkness, Game Over logic and increased gravity

---
+++ OutKast/Assets/Scripts/LightTimer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTimer : MonoBehaviour
+{
+    
+    
+    public float startTimer = 10;
+    public float walkingDecayMuliplier = 2;
+    public float RunningDecayMultiplier = 6;
+    public float startingSizeMultipliedByTimer = 1;
+    [HideInInspector] public float timer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = startTimer;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        gameObject.transform.localScale = new Vector3(timer * startingSizeMultipliedByTimer, timer * startingSizeMultipliedByTimer, 0);
+        bool isWalkingLeft = Input.GetKey(KeyCode.A);
+        bool isWalkingRight = Input.GetKey(KeyCode.D);
+        bool isRunning = (Input.GetKey(KeyCode.LeftShift) && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)));
+        if (isWalkingLeft || isWalkingRight)
+        {
+            timer -= (Time.deltaTime * walkingDecayMuliplier);
+        }
+        if (isRunning)
+        {
+            timer -= (Time.deltaTime * RunningDecayMultiplier);
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+}
