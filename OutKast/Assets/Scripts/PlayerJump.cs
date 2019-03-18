35d4d19ec2346519d02eb3c109822067c67a7ec0
Cedar Bishop <47874454+CedarBishop@users.noreply.github.com>
2019-03-18 12:12:09 +1100

    Added Double Jump functionality to Player Jump script, and set a max velocity for the y velocity so jumps are consistant

--- OutKast/Assets/Scripts/PlayerJump.cs
+++ OutKast/Assets/Scripts/PlayerJump.cs
@@ -4,38 +4,64 @@ public class PlayerJump : MonoBehaviour
 {
     private Rigidbody2D playerRB;
     public Transform transformEgo;
-    float groundCheckRadius = 0.15f;
     public LayerMask whatIsGround;
     [HideInInspector] public bool grounded;
-    bool hasJumped = false;
-    public float jumpForce;
-
+    private bool hasJumped = false;
+    float groundCheckRadius = 0.15f;
+    public float jumpForce = 100;
+    public int jumpsRemaining = 1;
+    public float jumpingSpeed = 1.5f;
+   
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
-    }        
-
-    void Update ()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
-        {
-            hasJumped = true;
-        }
     }
 
-    void FixedUpdate ()
+    void Update ()
+    {        
+        if (Input.GetKeyDown(KeyCode.Space))
+            hasJumped = true;
+        JumpingVelocityCap();
+    }
+
+    void FixedUpdate()
     {
         grounded = Physics2D.OverlapCircle(transformEgo.position, groundCheckRadius, whatIsGround);
-        if (hasJumped && grounded)
+        if (grounded && hasJumped)
         {
-            playerRB.AddForce(Vector2.up * jumpForce);
+            Jump();
             hasJumped = false;
         }
+        else if (grounded == false && hasJumped && jumpsRemaining > 0)
+        {
+            Jump();
+            hasJumped = false;
+            jumpsRemaining--;
+        }
+        else if (grounded)
+        {
+            jumpsRemaining = 1;
+        }
+
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transformEgo.position, groundCheckRadius);
+    }
+
+    void Jump()
+    {
+        playerRB.AddForce(Vector2.up * jumpForce);
+        Debug.Log("is using jump method. force: " + playerRB.velocity.y);
+    }
+
+
+    void JumpingVelocityCap()
+    {
+        float currentXVelocity = playerRB.velocity.x;
+        float cappedJumpingVelocity = Mathf.Min(Mathf.Abs(playerRB.velocity.y), jumpingSpeed) * Mathf.Sign(playerRB.velocity.y);
+        playerRB.velocity = new Vector2(currentXVelocity, cappedJumpingVelocity);
     }
 }
