5b5674cb494d0019a5f62da8454eb3967244c205
Tony G <ssj.tony89@gmail.com>
2019-03-26 16:49:32 +1100

    ?

---
+++ OutKast/Assets/Scripts/BetterJump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterJump : MonoBehaviour
+{
+    public float fallingGravityModifier = 2.5f;
+    Rigidbody2D playerRb;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerRb.velocity.y < 0)
+        {
+            playerRb.velocity += Vector2.up * Physics2D.gravity.y * (fallingGravityModifier - 1) * Time.deltaTime;
+        }
+    }
+}
