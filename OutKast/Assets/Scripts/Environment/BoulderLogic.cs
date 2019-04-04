20e4f96136a82cc0000cec61ed339a1323e80a59
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-04-05 10:28:35 +1100

    Created of leve one design

---
+++ OutKast/Assets/Scripts/Environment/BoulderLogic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderLogic : MonoBehaviour
+{
+    public GameObject boulder;
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            boulder.AddComponent<Rigidbody2D>();
+            gameObject.SetActive(false);
+        }
+    }
+}
