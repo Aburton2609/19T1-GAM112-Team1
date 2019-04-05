59879966ae2ac250eb078361f02c725ed94e9cb7
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-04-05 13:50:50 +1100

    Finished level 1 draft

--- OutKast/Assets/Scripts/Environment/BoulderLogic.cs
+++ OutKast/Assets/Scripts/Environment/BoulderLogic.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class BoulderLogic : MonoBehaviour
 {
     public GameObject boulder;
+    public float timeDelay = 0.5f;
 
     void OnTriggerEnter2D (Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            boulder.AddComponent<Rigidbody2D>();
-            gameObject.SetActive(false);
+            StartCoroutine("DelayBoulderFalling");
         }
+    }
+
+    IEnumerator DelayBoulderFalling ()
+    {
+        yield return new WaitForSeconds(timeDelay);
+        boulder.AddComponent<Rigidbody2D>();
+        gameObject.SetActive(false);
     }
 }
