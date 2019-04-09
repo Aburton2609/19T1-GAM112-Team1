b1c70c710ca17f6fa61f154509cdffba378197fb
Cedar Bishop <47874454+CedarBishop@users.noreply.github.com>
2019-04-09 14:35:54 +1000

    Created Pause Menu

---
+++ OutKast/Assets/Scripts/Game/PauseLogic.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseLogic : MonoBehaviour
+{
+    public Canvas pauseCanvas;
+    public string sceneName;
+    bool isPaused;
+    void Start()
+    {
+        pauseCanvas.gameObject.SetActive(false);
+        isPaused = false;
+    }
+
+    void Update()
+    {
+        if (isPaused == false && Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();           
+        }
+        else if (isPaused && Input.GetKeyDown(KeyCode.Escape))
+        {
+            ResumeGame();
+        }
+    }
+
+    void PauseGame ()
+    {
+        if (isPaused == false && Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            pauseCanvas.gameObject.SetActive(true);
+            isPaused = true;
+        }
+    }
+
+    public void ResumeGame ()
+    {
+        Time.timeScale = 1;
+        pauseCanvas.gameObject.SetActive(false);
+        isPaused = false;
+    }
+
+    public void ReturnToMainMenu ()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
