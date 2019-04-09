1a6bf0b15134e48b233cbdcc99f5694c0578ab4a
Cedar Bishop <47874454+CedarBishop@users.noreply.github.com>
2019-04-09 15:38:54 +1000

    Added Serialization to completing the level which determines if a level is locked, unlocked or beaten

--- OutKast/Assets/Scripts/MainMenu/MainMenuLogic.cs
+++ OutKast/Assets/Scripts/MainMenu/MainMenuLogic.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuLogic : MonoBehaviour
@@ -8,11 +9,37 @@ public class MainMenuLogic : MonoBehaviour
     public string[] sceneNames;
     public Canvas playQuitCanvas;
     public Canvas levelSelectionCanvas;
+    public Button[] levelSelectionsButtons;
 
     private void Start()
     {
         playQuitCanvas.gameObject.SetActive(true);
         levelSelectionCanvas.gameObject.SetActive(false);
+
+        if (PlayerPrefs.GetString("LevelThreeCompletionStatus") == "Complete")
+        {
+            levelSelectionsButtons[0].GetComponent<Image>().color = Color.green;
+            levelSelectionsButtons[1].GetComponent<Image>().color = Color.green;
+            levelSelectionsButtons[2].GetComponent<Image>().color = Color.green;
+        }
+        else if (PlayerPrefs.GetString("LevelTwoCompletionStatus") == "Complete")
+        {
+            levelSelectionsButtons[0].GetComponent<Image>().color = Color.green;
+            levelSelectionsButtons[1].GetComponent<Image>().color = Color.green;
+            levelSelectionsButtons[2].GetComponent<Image>().color = Color.white;
+        }
+        else if (PlayerPrefs.GetString("LevelOneCompletionStatus") == "Complete")
+        {
+            levelSelectionsButtons[0].GetComponent<Image>().color = Color.green;
+            levelSelectionsButtons[1].GetComponent<Image>().color = Color.white;
+            levelSelectionsButtons[2].GetComponent<Image>().color = Color.grey;
+        }
+        else
+        {
+            levelSelectionsButtons[0].GetComponent<Image>().color = Color.white;
+            levelSelectionsButtons[1].GetComponent<Image>().color = Color.grey;
+            levelSelectionsButtons[2].GetComponent<Image>().color = Color.grey;
+        }        
     }
 
     public void PlayGame ()
@@ -23,18 +50,23 @@ public class MainMenuLogic : MonoBehaviour
 
     public void LoadLevelOne()
     {
-        SceneManager.LoadScene(sceneNames[0]);
+        SceneManager.LoadScene(sceneNames[0]);       
     }
 
     public void LoadLevelTwo()
-    {        
-
-         SceneManager.LoadScene(sceneNames[1]);        
+    {
+        if (PlayerPrefs.GetString("LevelOneCompletionStatus") == "Complete")
+        {
+            SceneManager.LoadScene(sceneNames[1]);
+        }             
     }
 
     public void LoadLevelThree()
     {
-        SceneManager.LoadScene(sceneNames[2]);
+        if (PlayerPrefs.GetString("LevelTwoCompletionStatus") == "Complete")
+        {
+            SceneManager.LoadScene(sceneNames[2]);
+        }
     }
 
     public void Return ()
