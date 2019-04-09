8095abfc3507c67c120dc9aebdea6554c150d96b
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-04-09 19:21:47 +1000

    Fixed parallax namespace error and pause menu canvas in other scenes

--- OutKast/Assets/Scripts/Environment/Parallax.cs
+++ OutKast/Assets/Scripts/Environment/Parallax.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.UIElements;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
