b63e26f087e85305c0020ad9c768c9625304e597
CedarBishop <47874454+CedarBishop@users.noreply.github.com>
2019-04-15 22:18:11 +1000

    Added boulder shake and restart level button in pause

--- OutKast/Assets/Scripts/Environment/BoulderLogic.cs
+++ OutKast/Assets/Scripts/Environment/BoulderLogic.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BoulderLogic : MonoBehaviour
 {
     public GameObject boulder;
     public float timeDelay = 0.5f;
+    public float shakeAmount = 0.1f;
+    Vector3 originalPosition;
+
+    void Start ()
+    {
+        originalPosition = boulder.transform.position;
+    }
+
 
     void OnTriggerEnter2D (Collider2D other)
     {
@@ -17,8 +24,30 @@ public class BoulderLogic : MonoBehaviour
 
     IEnumerator DelayBoulderFalling ()
     {
+        InvokeRepeating("BeginShake", 0, 0.01f);        
         yield return new WaitForSeconds(timeDelay);
         boulder.AddComponent<Rigidbody2D>();
+        Invoke("StopShake",0);
         gameObject.SetActive(false);
+    }
+
+    void BeginShake()
+    {
+        if (shakeAmount > 0)
+        {
+            
+            Vector3 newPosition = boulder.transform.position;
+            float shakeAmountX = Random.value * shakeAmount * 2 - shakeAmount;
+            float shakeAmountY = Random.value * shakeAmount * 2 - shakeAmount;
+            newPosition.x += shakeAmountX;
+            newPosition.y += shakeAmountY;
+            boulder.transform.position = newPosition;
+        }
+    }
+
+    void StopShake()
+    {
+        CancelInvoke("BeginShake");
+        boulder.transform.position = originalPosition;
     }
 }
